be229b12398f4b764e01a89c6a2969377e01a0aa
jlllorente3 <joseluis.llorenteperales+3@gmail.com>
2025-02-26 17:17:40 +0100

    Test VSCode

--- src/main/java/es/cursojava/Main.java
+++ src/main/java/es/cursojava/Main.java
@@ -7,10 +7,10 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
-        logger.debug("Debug Hello world!");
-        logger.info("Info Hello world!");
-        logger.warn("Warn Hello world!");
-        logger.error("Error Hello world!");
+        logger.debug("Debug Hello world2!");
+        logger.info("Info Hello world2!");
+        logger.warn("Warn Hello world2!");
+        logger.error("Error Hello world2!");
         
     }
 }
